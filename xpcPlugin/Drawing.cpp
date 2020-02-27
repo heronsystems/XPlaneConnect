602931aaf0b4c42a2b73aa1bae4a14e4d3acca40
Madison Blake <madison.blake@heronsystems.com>
2020-02-27 10:23:29 -0500

    Compiles for windows

--- xpcPlugin/Drawing.cpp
+++ xpcPlugin/Drawing.cpp
@@ -14,11 +14,13 @@
 //     may be used to endorse or promote products derived from this software
 //     without specific prior written permission from the authors or
 //     Laminar Research, respectively.
+#define _USE_MATH_DEFINES
 #include "Drawing.h"
 
 #include "XPLMDisplay.h"
 #include "XPLMGraphics.h"
 #include "XPLMDataAccess.h"
+
 
 #include <cmath>
 #include <string>
@@ -28,8 +30,9 @@
 
 #define GL_GLEXT_PROTOTYPES
 #if IBM
-#include <windows.h>
+#  include <windows.h>
 #endif
+
 #ifdef __APPLE__
 #  include <OpenGL/gl.h>
 #else
@@ -40,16 +43,9 @@
 #include <chrono>
 #include <cstdint>
 
-#include "./common/shader_utils.h"
-
 #include <Eigen/Dense>
 #include <Eigen/Geometry> 
 
-#include <ft2build.h>
-#include FT_FREETYPE_H
-
-FT_Library ft;
-FT_Face face;
 
 GLuint vbo;
 
@@ -158,69 +154,6 @@ namespace XPC
 		return 0;
 	}
 
-
-	static void render_text(const char *text, float x, float y, float sx, float sy) {
-		const char *p;
-		FT_GlyphSlot g = face->glyph;
-
-		/* Create a texture that will be used to hold one "glyph" */
-		GLuint tex;
-
-		glActiveTexture(GL_TEXTURE0);
-		glGenTextures(1, &tex);
-		glBindTexture(GL_TEXTURE_2D, tex);
-		glUniform1i(uniform_tex, 0);
-
-		/* We require 1 byte alignment when uploading texture data */
-		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-		/* Clamping to edges is important to prevent artifacts when scaling */
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-		/* Linear filtering usually looks best for text */
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-		/* Set up the VBO for our vertex data */
-		glEnableVertexAttribArray(attribute_coord);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glVertexAttribPointer(attribute_coord, 4, GL_FLOAT, GL_FALSE, 0, 0);
-		/* Loop through all characters */
-		for (p = text; *p; p++) {
-			/* Try to load and render the character */
-			if (FT_Load_Char(face, *p, FT_LOAD_RENDER))
-				continue;
-
-			/* Upload the "bitmap", which contains an 8-bit grayscale image, as an alpha texture */
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, g->bitmap.width, g->bitmap.rows, 0, GL_ALPHA, GL_UNSIGNED_BYTE, g->bitmap.buffer);
-
-			/* Calculate the vertex and texture coordinates */
-			float x2 = x + g->bitmap_left * sx;
-			float y2 = -y - g->bitmap_top * sy;
-			float w = g->bitmap.width * sx;
-			float h = g->bitmap.rows * sy;
-
-			point box[4] = {
-				{x2, -y2, 0, 0},
-				{x2 + w, -y2, 1, 0},
-				{x2, -y2 - h, 0, 1},
-				{x2 + w, -y2 - h, 1, 1},
-			};
-
-			/* Draw the character on the screen */
-			glBufferData(GL_ARRAY_BUFFER, sizeof box, box, GL_DYNAMIC_DRAW);
-			glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-			/* Advance the cursor to the start of the next character */
-			x += (g->advance.x >> 6) * sx;
-			y += (g->advance.y >> 6) * sy;
-		}
-
-		glDisableVertexAttribArray(attribute_coord);
-		glDeleteTextures(1, &tex);
-
-	}
 
 	static float makerSize(float d) {
 		const float TAN = 0.00436335F;
@@ -457,9 +390,9 @@ namespace XPC
 				strncpy(v2, msg2, len2 + 1);
 				XPLMDrawString(rgb2, -50 + pos3D_x, pos3D_y-10, v2, NULL, xplmFont_Proportional);
 			}
-			
-			return 1;
 		}
+
+		return 0;
 	}
 
 	/// Draws waypoints.
@@ -517,44 +450,7 @@ namespace XPC
 			glColor3f(1.0F, f, f);
 			gl_drawCube((float)l->x, (float)l->y, (float)l->z, d);
 		}
-
-		// int screen_width=800, screen_height=600;
-		// float sx = 2.0 / screen_width;
-		// float sy = 2.0 / screen_height;
-
-
-		// if(FT_Init_FreeType(&ft)) {
-		// 	fprintf(stderr, "Could not init freetype library\n");
-		// 	return 1;
-		// }
-
-		
-		// if(FT_New_Face(ft, "Resources/plugins/XPlaneConnect/FreeSans.ttf", 0, &face)) {
-		// 	fprintf(stderr, "Could not open font\n");
-		// 	return 1;
-		// }
-		// program = create_program("Resources/plugins/XPlaneConnect/text.v.glsl", "Resources/plugins/XPlaneConnect/text.f.glsl");
-		// if(program == 0)
-		// 	return 1;
-
-		// attribute_coord = get_attrib(program, "coord");
-		// uniform_tex = get_uniform(program, "tex");
-		// uniform_color = get_uniform(program, "color");
-
-		// if(attribute_coord == -1 || uniform_tex == -1 || uniform_color == -1)
-		// 	return false;
-
-		// // Create the vertex buffer object
-		// glGenBuffers(1, &vbo);
-
-		// GLfloat black[4] = { 0, 0, 0, 1 };
-		// GLfloat red[4] = { 1, 0, 0, 1 };
-		// GLfloat transparent_green[4] = { 0, 1, 0, 0.5 };
-
-
-		// FT_Set_Pixel_Sizes(face, 0, 48);
-		// glUniform4fv(uniform_color, 1, black);
-		// render_text("AAAAAAAAAAAAAAAA", -1 + 8 * sx, 1 - 50 * sy, sx, sy);
+		return 0;
 	}
 
 
